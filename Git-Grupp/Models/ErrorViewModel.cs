9b3ea01078f647faae44230705ecb3d4fdde3eab
emilk <emilk@LAPTOP-B1TBTE4C>
2022-07-06 16:16:49 +0200

    Add project files.

---
+++ Git-Grupp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Git_Grupp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
