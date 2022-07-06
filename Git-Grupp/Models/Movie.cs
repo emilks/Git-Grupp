b11805ad2fd82f4a03dab0e9c3f81f2eec39997e
Klara Höglund <klarahoglund@hotmail.com>
2022-07-06 16:48:54 +0200

    Ett test till

---
+++ Git-Grupp/Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿namespace Git_Grupp.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+    }
+}
