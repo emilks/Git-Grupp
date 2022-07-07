cec79c17cb8ee4aff69993d15d3e50b100254045
eneridahunda <eneridahunda@gmail.com>
2022-07-07 11:10:22 +0200

    New

---
+++ Git-Grupp/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿namespace Git_Grupp.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        //Det här är den ändringen jag gjort nu kl 09.30
+        /// <summary>
+        /// Det här en ny ändringar
+        /// </summary>
+        public float Rating { get; set; }
+        public int Type { get; set; }
+
+        //Nu gör jag en kommentar som vi kallar för Oscar
+
+        public string Name { get; set; }
+
+    }
+}
