d2ef57305f76e8647a0fbafd997fc15738b0c0fa
Klara Höglund <klarahoglund@hotmail.com>
2022-07-07 09:31:03 +0200

    En ändring i Movies

---
+++ Git-Grupp/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿namespace Git_Grupp.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+        public float Rating { get; set; }
+
+    }
+}
