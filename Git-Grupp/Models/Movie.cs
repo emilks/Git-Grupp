b875e27ab7b4633e95e27355f4e2e9a0ffb71ba1
Klara Höglund <klarahoglund@hotmail.com>
2022-07-07 09:32:51 +0200

    Detta är med kommentar i Movie

--- Git-Grupp/Models/Movie.cs
+++ Git-Grupp/Models/Movie.cs
@@ -7,6 +7,8 @@
         public string? Title { get; set; }
 
         public DateTime CreatedDate { get; set; }
+
+        //Det här är den ändringen jag gjort nu kl 09.30
         public float Rating { get; set; }
         public int Type { get; set; }
 
